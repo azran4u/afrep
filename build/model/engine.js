0cdc8ab5fed2c62affb3b2dc04482912b9dfe5ec
Eyal Azran <azran4u@gmail.com>
2020-09-23 22:51:34 +0300

    update gitignore

--- build/model/engine.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var Engine = /** @class */ (function () {
-  function Engine(seq) {
-    this.seq = seq;
-  }
-  return Engine;
-})();
-exports.Engine = Engine;
